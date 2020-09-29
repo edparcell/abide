f439010f377bcd68d85eed808e46e71aba0202ce
Ed Parcell <edparcell@gmail.com>
2020-09-29 16:05:57 -0600

    Added scheduler class

--- abide/schedule.py
+++ abide/schedule.py
@@ -53,3 +53,37 @@ def get_next_run(now: datetime, job_defn: ScheduledJobDefinition, job_state: Sch
         break
 
     return job_time, (job_time, 0)
+
+class Scheduler:
+    def __init__(self, start_time, job_definitions=None):
+        self.job_definitions = job_definitions or {}
+        self.states = {}
+        self.time = start_time
+
+    def add_job(self, name, job_definition):
+        self.job_definitions[name] = job_definition
+
+    def set_time(self, time):
+        self.time = time
+
+    def set_running(self, name, run, retry):
+        self.states[name] = ScheduledJobState(run, retry, None, RunState.RUNNING)
+
+    def set_complete(self, name, state):
+        current_job_state = self.states[name]
+        self.states[name] = ScheduledJobState(current_job_state.last_run, current_job_state.last_retry, self.time, state)
+
+    def get_next_run(self):
+        res_when = None
+        res_job_name = None
+        res_job_time = None
+        res_retry = None
+        for name, job_definition in self.job_definitions.items():
+            job_state = self.states.get(name)
+            when, (job_time, retry) = get_next_run(self.time, job_definition, job_state)
+            if res_when is None or when < res_when:
+                res_when = when
+                res_job_name = name
+                res_job_time = job_time
+                res_retry = retry
+        return res_job_name, res_when, (res_job_time, res_retry)
--- abide/test/test_schedule.py
+++ abide/test/test_schedule.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from abide.schedule import ScheduledJobDefinition, get_next_run, ScheduledJobState, RunState
+from abide.schedule import ScheduledJobDefinition, get_next_run, ScheduledJobState, RunState, Scheduler
 
 
 def test_basic():
@@ -117,3 +117,81 @@ def test_get_next_if_running():
     when, (job_time, job_retry) = get_next_run(now, sjd, sjs)
     assert job_time == when == pd.to_datetime('9/28/2020 13:15:00')
     assert job_retry == 0
+
+
+def test_scheduler_basic():
+    s = Scheduler(pd.to_datetime('9/29/2020 14:05'),
+                  {
+                      'A': ScheduledJobDefinition("*/5 * * * *", late_start_cutoff=0),
+                      'B': ScheduledJobDefinition("*/12 * * * *", late_start_cutoff=0),
+                  })
+
+    job_name, when, (job_time, retry) = s.get_next_run()
+    assert job_name == 'A'
+    assert when == pd.to_datetime('9/29/2020 14:05')
+    assert job_time == pd.to_datetime('9/29/2020 14:05')
+    assert retry == 0
+
+    s.set_time(when)
+    s.set_running(job_name, job_time, retry)
+
+    s.set_time(pd.to_datetime('9/29/2020 14:06:30'))
+    s.set_complete('A', RunState.COMPLETE)
+
+    job_name, when, (job_time, retry) = s.get_next_run()
+    assert job_name == 'A'
+    assert when == pd.to_datetime('9/29/2020 14:10')
+    assert job_time == pd.to_datetime('9/29/2020 14:10')
+    assert retry == 0
+
+
+def test_scheduler_retry_on_fail_no_retries():
+    s = Scheduler(pd.to_datetime('9/29/2020 14:05'),
+                  {
+                      'A': ScheduledJobDefinition("*/5 * * * *", late_start_cutoff=0),
+                      'B': ScheduledJobDefinition("*/12 * * * *", late_start_cutoff=0),
+                  })
+
+    job_name, when, (job_time, retry) = s.get_next_run()
+    assert job_name == 'A'
+    assert when == pd.to_datetime('9/29/2020 14:05')
+    assert job_time == pd.to_datetime('9/29/2020 14:05')
+    assert retry == 0
+
+    s.set_time(when)
+    s.set_running(job_name, job_time, retry)
+
+    s.set_time(pd.to_datetime('9/29/2020 14:06:30'))
+    s.set_complete('A', RunState.FAIL)
+
+    job_name, when, (job_time, retry) = s.get_next_run()
+    assert job_name == 'A'
+    assert when == pd.to_datetime('9/29/2020 14:10')
+    assert job_time == pd.to_datetime('9/29/2020 14:10')
+    assert retry == 0
+
+
+def test_scheduler_retry_on_fail_three_retries():
+    s = Scheduler(pd.to_datetime('9/29/2020 14:05'),
+                  {
+                      'A': ScheduledJobDefinition("*/5 * * * *", late_start_cutoff=0, retries=3),
+                      'B': ScheduledJobDefinition("*/12 * * * *", late_start_cutoff=0),
+                  })
+
+    job_name, when, (job_time, retry) = s.get_next_run()
+    assert job_name == 'A'
+    assert when == pd.to_datetime('9/29/2020 14:05')
+    assert job_time == pd.to_datetime('9/29/2020 14:05')
+    assert retry == 0
+
+    s.set_time(when)
+    s.set_running(job_name, job_time, retry)
+
+    s.set_time(pd.to_datetime('9/29/2020 14:06:30'))
+    s.set_complete('A', RunState.FAIL)
+
+    job_name, when, (job_time, retry) = s.get_next_run()
+    assert job_name == 'A'
+    assert when == pd.to_datetime('9/29/2020 14:06:30')
+    assert job_time == pd.to_datetime('9/29/2020 14:05')
+    assert retry == 1
